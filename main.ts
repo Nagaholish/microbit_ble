663f4a80d3de265517ee934ad125d614199e671d
Nagaholish <horogolohorogoli@gmail.com>
2023-03-26 22:57:33 +0900

    ピンのテストをコミットする

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+bluetooth.onBluetoothConnected(function () {
+    basic.showLeds(`
+        # # # . .
+        # . . # .
+        # # # . .
+        # . . # .
+        # # # # .
+        `)
+})
 input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
         . . # . .
@@ -12,8 +21,12 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
         lastValue = control.eventValue()
         if (control.eventValue() == 1) {
             basic.showString("A")
+            // 脚・前
+            pins.digitalWritePin(DigitalPin.P0, 0)
         } else if (control.eventValue() == 3) {
             basic.showString("B")
+            // 脚・後
+            pins.digitalWritePin(DigitalPin.P0, 0)
         } else if (control.eventValue() == 5) {
             basic.showString("C")
         } else if (control.eventValue() == 7) {
@@ -22,12 +35,18 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
             basic.showString("1")
         } else if (control.eventValue() == 11) {
             basic.showString("2")
+            // 腰・反時計
+            pins.digitalWritePin(DigitalPin.P16, 0)
+            // 腰・時計
+            pins.digitalWritePin(DigitalPin.P15, 1)
         } else if (control.eventValue() == 13) {
             basic.showString("3")
+            pins.analogWritePin(AnalogPin.P15, 1023)
         } else if (control.eventValue() == 15) {
             basic.showString("4")
+            pins.analogWritePin(AnalogPin.P15, 520)
         } else {
-        	
+            pins.analogWritePin(AnalogPin.P15, 0)
         }
     }
 })
