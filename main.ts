8807e4a598372fbc2a243bd2cb76af6bad676175
Nagaholish <horogolohorogoli@gmail.com>
2023-03-31 22:56:41 +0900

    BLE接続／切断でアイコンを変更・赤外線センサ処理を追加・コントローラー入力if部分を整理

--- main.ts
+++ main.ts
@@ -7,12 +7,24 @@ function 前進する () {
 }
 bluetooth.onBluetoothConnected(function () {
     basic.showLeds(`
+        # . . . #
+        # # . # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
         . . . . .
         # # # # #
-        # . # . #
-        . # # # .
         . # # # .
         `)
+})
+input.onButtonPressed(Button.A, function () {
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
 })
 function 全停止する () {
     pins.digitalWritePin(DigitalPin.P14, 0)
@@ -20,32 +32,23 @@ function 全停止する () {
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P16, 0)
 }
+input.onButtonPressed(Button.B, function () {
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.mysterious), SoundExpressionPlayMode.UntilDone)
+})
 function 後退する () {
-    pins.digitalWritePin(DigitalPin.P14, 0)
-    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P13, 0)
 }
 control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
     if (lastValue != control.eventValue()) {
         lastValue = control.eventValue()
         if (control.eventValue() == 1) {
-            basic.showString("A")
             前進する()
         } else if (control.eventValue() == 3) {
-            basic.showString("B")
             後退する()
-        } else if (control.eventValue() == 5) {
-            basic.showString("C")
-        } else if (control.eventValue() == 7) {
-            basic.showString("D")
-        } else if (control.eventValue() == 9) {
-            basic.showString("1")
-        } else if (control.eventValue() == 11) {
-            basic.showString("2")
         } else if (control.eventValue() == 13) {
-            basic.showString("3")
             反時計回り回転する()
         } else if (control.eventValue() == 15) {
-            basic.showString("4")
             時計回り回転する()
         } else {
             全停止する()
@@ -55,10 +58,11 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
 function 時計回り回転する () {
     pins.digitalWritePin(DigitalPin.P15, 1)
 }
+let 赤外線左 = 0
+let 赤外線右 = 0
 let lastValue = 0
 lastValue = 0
 bluetooth.startLEDService()
-bluetooth.startAccelerometerService()
 bluetooth.startButtonService()
 basic.showLeds(`
     . . . . .
@@ -67,3 +71,10 @@ basic.showLeds(`
     . . # . .
     . . . . .
     `)
+basic.forever(function () {
+    赤外線右 = pins.digitalReadPin(DigitalPin.P1)
+    赤外線左 = pins.digitalReadPin(DigitalPin.P2)
+    if (赤外線右 == 1 || 赤外線左 == 1) {
+        後退する()
+    }
+})
